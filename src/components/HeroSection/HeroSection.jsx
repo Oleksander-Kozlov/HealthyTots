56da0a2e90ed08742acbb2ebf4312d46f8aae4d7
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-12-05 15:09:36 +0300

    add img block with styles
    add png, img block and styles

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,17 @@
-
+import Hero_img1 from "../../assets/Hero_img1.png";
+import Hero_img2 from "../../assets/Hero_img2.png";
+import Hero_img3 from "../../assets/Hero_img3.png";
+import Hero_img4 from "../../assets/Hero_img4.png";
+import Hero_img5 from "../../assets/img55.png";
 import { Container_Wrapper } from "../../pages/HomePage/HomePage.styled";
 import { 
-
+  About_us_Image,
   H1,
   HeroBtn,
   Hero_Wrapper,
-  
+  IMG_Block_Wrapper,
+  IMG_Wrapper,
+  IMG_Wrapper_Center,
 } from "./HeroSection.styled";
 
 const HeroSection = () => {
@@ -31,7 +37,23 @@ const HeroSection = () => {
           </H1>
           <HeroBtn>Записатися на прийом</HeroBtn>
         </Hero_Wrapper>
-      
+        <IMG_Block_Wrapper>
+          <IMG_Wrapper x="left" y="top">
+            <About_us_Image src={Hero_img1} alt="Hero_img1" />
+          </IMG_Wrapper>
+          <IMG_Wrapper x="right" y="top">
+            <About_us_Image src={Hero_img2} alt="Hero_img2" />
+          </IMG_Wrapper>
+          <IMG_Wrapper x="left" y="bottom">
+            <About_us_Image src={Hero_img3} alt="Hero_img3" />
+          </IMG_Wrapper>
+          <IMG_Wrapper x="right" y="bottom">
+            <About_us_Image src={Hero_img4} alt="Hero_img4" />
+          </IMG_Wrapper>
+          <IMG_Wrapper_Center>
+            <About_us_Image src={Hero_img5} alt="Hero_img5" />
+          </IMG_Wrapper_Center>
+        </IMG_Block_Wrapper>
       </Container_Wrapper>
     </section>
   );
--- src/components/HeroSection/HeroSection.styled.js
+++ src/components/HeroSection/HeroSection.styled.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-
+import Hero_img5 from "../../assets/img55.png";
 
 export const Hero_Wrapper = styled.div`
 display: flex;flex-direction:column;
@@ -31,3 +31,81 @@ export const HeroBtn = styled.button`
   font-weight: 400;
   line-height: normal;
 padding: 16px 32px`;
+
+export const IMG_Block_Wrapper = styled.div`
+  position: relative;
+
+  width: 584px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 24px;
+  /* background-color: gray; */
+  border-radius: 20px;
+`;
+
+export const IMG_Wrapper_Center = styled.div`
+  position: absolute;
+  top: 24%;
+  left: 24%;
+  width: 280px;
+  /* z-index: 199; */
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: -50%;
+    left: -50%;
+    background-image: url(${Hero_img5});
+    background-repeat: no-repeat;
+    background-size: contain;
+    border-radius: 20px;
+
+    opacity: 0; /* Начальная прозрачность */
+    transition: opacity 0.3s ease; /* Плавное появление */
+  }
+  &:hover::before {
+    opacity: 1;
+    width: 584px;
+    height: 584px;
+    z-index: 100;
+    animation: ${zoomAnimation} 0.8s forwards;
+  }
+`; 
+
+
+// Создание ключевых кадров для анимации
+const zoomAnimation = keyframes`
+  0% {
+    transform: scale(1); /* Исходный размер */
+  }
+  50% {
+    transform: scale(2.2); /* Увеличение в 2 раза */
+  }
+  100% {
+    transform: scale(1); /* Возврат к исходному размеру */
+  }
+`;
+export const IMG_Wrapper = styled.div`
+  position: relative;
+
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  transform-origin: ${(props) => props.x} ${(props) => props.y};
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(2);
+    /* z-index: 100; */
+    background-color: transparent;
+    animation: ${zoomAnimation} 0.8s forwards;
+  }
+`;
+
+export const About_us_Image = styled.img`
+  width: 280px;
+  height: 280px;
+  border-radius: 20px;
+`;
