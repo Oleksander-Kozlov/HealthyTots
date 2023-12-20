c5e63cb318d940266e07c04957a414d0ad083dde
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-12-20 02:33:21 +0200

    fix indentation in adress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
           <Contact_container href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9D%D0%B0%D0%BA%D0%BE%D0%BD%D0%B5%D1%87%D0%BD%D0%BE%D0%B3%D0%BE,+131%D0%84,
           +%D0%9B%D0%B0%D0%B4%D1%8B%D0%B6%D0%B8%D0%BD,+%D0%92%D0%B8%D0%BD%D0%BD%D0%B8%D1%86%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,
           +%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0,+24320/@48.6865952,29.2400918,17z/data=!3m1!4b1!4m6!3m5!1s0x40cd706198053a0d:0x17fc425a24df00ec!8m2!3d48.
-          6865917!4d29.2426667!16s%2Fg%2F11t3_f2_tj?entry=ttu"><TiLocation color="#8CD4FA" size={24} /><p>м.Ладижин,Наконечного 131Є</p>
+          6865917!4d29.2426667!16s%2Fg%2F11t3_f2_tj?entry=ttu"><TiLocation color="#8CD4FA" size={24} /><p>м.Ладижин, Наконечного 131Є</p>
         </Contact_container>
         <img src={logoHT} alt="logoHT" />
           <p>
