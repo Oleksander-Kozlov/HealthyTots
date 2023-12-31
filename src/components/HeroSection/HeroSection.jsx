1f0e09fbeba6e1488fe793440c0162b3756dde02
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-01-01 00:44:04 +0200

    added mob and tab adaptive stylre for HeroSection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import Hero_img2 from "../../assets/rectangle/Rectangle1_1x.png";
-import Hero_img3  from "../../assets/rectangle/Rectangle2_1x.png";
+import Hero_img3 from "../../assets/rectangle/Rectangle2_1x.png";
 import Hero_img1 from "../../assets/rectangle/Rectangle3_1x.png";
 import Hero_img4 from "../../assets/rectangle/Rectangle4_1x.png";
 import Hero_img5 from "../../assets/rectangle/RectangleCenter_1x.png";
@@ -7,10 +7,12 @@ import Hero_img5 from "../../assets/rectangle/RectangleCenter_1x.png";
 import MakeAppointmentButton from "../MakeAppointmentButton/MakeAppointmentButton";
 import {
   About_us_Image,
+  BackgroundBlur,
   // BouncyDiv,
   H1,
+  H1andButton_Wrapper,
   Hero_Container_Wrapper,
-  Hero_Wrapper,
+  
   IMG_Block_Wrapper,
   IMG_Wrapper,
   IMG_Wrapper_Center,
@@ -18,12 +20,12 @@ import {
 } from "./HeroSection.styled";
 import PropTypes from "prop-types";
 
-
-const HeroSection = ({handleClick}) => {
+const HeroSection = ({ handleClick }) => {
   return (
     <Section>
       <Hero_Container_Wrapper>
         <IMG_Block_Wrapper>
+          <BackgroundBlur></BackgroundBlur>
           {/* <img
                   srcset="./images\portfolio\img1_356.jpg 356w,
                 ./images\portfolio\img1_360.jpg 360w,
@@ -40,7 +42,13 @@ const HeroSection = ({handleClick}) => {
                   height="280"/> */}
 
           <IMG_Wrapper x="left" y="top">
-            <About_us_Image src={Hero_img1} alt="Hero_img1" />
+            {/* <About_us_Image src={Hero_img1} alt="Hero_img1" /> */}
+            <img
+              srcSet={`${Hero_img3} 152w,    ${Hero_img4} 302w,   ${Hero_img1} 280w,   ${Hero_img2} 560w  `}
+              sizes="(max-width: 1439.98px) 152px, 280px"
+              src={Hero_img5}
+              alt="Toys for child in clinic"
+            />
           </IMG_Wrapper>
           <IMG_Wrapper x="right" y="top">
             <About_us_Image src={Hero_img2} alt="Hero_img2" />
@@ -55,8 +63,8 @@ const HeroSection = ({handleClick}) => {
             <About_us_Image src={Hero_img5} alt="Hero_img_center" />
           </IMG_Wrapper_Center>
         </IMG_Block_Wrapper>
-        <Hero_Wrapper>
-          <H1 style={{ fontSize: "48px", color: "#0D3F7C", textAlign: "left" }}>
+        <H1andButton_Wrapper>
+          <H1>
             Healthy Tots: де кожен<br></br> маленький пацієнт - великий
             пріоритет.
           </H1>
@@ -65,13 +73,12 @@ const HeroSection = ({handleClick}) => {
             Записатися на прийом
           </MakeAppointmentButton>
           {/* </BouncyDiv> */}
-        </Hero_Wrapper>
+        </H1andButton_Wrapper>
       </Hero_Container_Wrapper>
     </Section>
   );
 };
-  HeroSection.propTypes = {
-  
-    handleClick: PropTypes.func.isRequired,
-  };
+HeroSection.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 export default HeroSection;
--- src/components/HeroSection/HeroSection.styled.js
+++ src/components/HeroSection/HeroSection.styled.js
@@ -1,6 +1,7 @@
 import styled, { keyframes } from "styled-components";
 import { pulse } from "react-animations";
 export const Section = styled.section`
+position: relative;
   padding-top: 20px;
   padding-bottom: 20px;
   margin-top: 36px;
@@ -18,38 +19,65 @@ export const Hero_Container_Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  @media screen and (min-width: 768px) {
+  /* align-items: center; */
+
+  padding-left: 20px;
+  padding-right: 20px;
+  width: 360px;
+  @media screen and (min-width: 768px) and (max-width: 1339.98px) {
+    padding-left: 40px;
+    padding-right: 40px;
+    flex-direction: row-reverse;
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1440px) {
     display: flex;
-    flex-direction: row;
+    flex-direction: row-reverse;
     justify-content: space-between;
     gap: 24px;
-    width: 768px;
-    padding: 40px 120px;
+    width: 1200px;
+    /* padding: 40px 120px; */
     overflow: hidden;
   }
 `;
-export const Hero_Wrapper = styled.div`
-  width: 590px;
-
+export const H1andButton_Wrapper = styled.div`
+  width: 360px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  text-align: left;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    width: 590px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: left;
+  }
 `;
 
 export const H1 = styled.h1`
-  margin-bottom: 48px;
-  color: #000;
-  text-align: left;
-
+  width: 310px;
   color: #0d3f7c;
-  /* Header1 */
+  text-align: left;
   font-family: Balsamiq Sans;
-  font-size: 48px;
+  font-size: 28px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
+  margin-bottom: 28px;
+
+  @media screen and (min-width: 1440px) {
+    margin-bottom: 48px;
+    width: auto;
+
+    text-align: left;
+
+    /* Header1 */
+
+    font-size: 48px;
+  }
 `;
 const pulseAnimation = keyframes`${pulse}`;
 
@@ -57,31 +85,44 @@ export const BouncyDiv = styled.div`
   animation: 1s ${pulseAnimation} infinite;
 `;
 export const HeroBtn = styled.button`
-  width: 260px;
-  border-radius: 20px;
-  background: #0d3f7c;
   color: #fff;
-  font-family: Inter;
-  font-size: 24px;
+  font-family: Balsamiq Sans;
+  font-size: 18px;
   font-style: normal;
-  font-weight: 400;
+  font-weight: 700;
   line-height: normal;
-  padding: 16px 32px;
+
+  @media screen and (min-width: 1440px) {
+    width: 260px;
+    border-radius: 20px;
+    background: #0d3f7c;
+    color: #fff;
+
+    font-size: 24px;
+
+    font-weight: 700;
+
+    padding: 16px 32px;
+  }
 `;
 
 export const IMG_Block_Wrapper = styled.div`
   position: relative;
-  width: 360px;
+  /* width: 332px; */
 
   display: flex;
+
   justify-content: center;
 
   flex-direction: row;
   flex-wrap: wrap;
   gap: 16px;
-  @media screen and (min-width: 768px) {
-    width: 584px;
-    gap: 24px;
+  margin-bottom: 28px;
+
+  @media screen and (min-width: 1440px) {
+    width: 590px;
+    gap: 28px;
+   
   }
 `;
 // Создание ключевых кадров для анимации
@@ -115,38 +156,69 @@ const zoomAnimation1 = keyframes`
     transform: scale(2.2);/* Возврат к исходному размеру */
   };
   `;
-
+export const BackgroundBlur = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* background-color: rgba(200, 212, 181, 0.8); */
+  backdrop-filter: blur(8px); /* початкове значення - без розмиття */
+  z-index: -1; /* фон повинен бути за зображенням */
+  transition: backdrop-filter 0.3s ease; /* для плавного змінювання ефекту розмиття */
+`;
 export const IMG_Wrapper_Center = styled.div`
   position: absolute;
   top: 26%;
   left: 26%;
-  width: 280px;
-
+  width: 158px;
+  @media screen and (max-width: 359.98px) {
+    width: 150px;
+  }
+  &:hover ${BackgroundBlur} {
+    /* backdrop-filter: blur(8px); */
+    z-index: 101;
+  }
   &:hover {
-    z-index: 100;
-    box-shadow: 0px 0px 16px 16px rgba(13, 63, 124, 0.25);
+    z-index: 102;
+    box-shadow: 0px 0px 8px 8px rgba(13, 63, 124, 0.25);
     border-radius: 20px;
 
     background-color: transparent;
-    animation: ${zoomAnimation1} 2s 500ms both;
+    scale: 1.5;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 280px;
+    &:hover {
+      animation: ${zoomAnimation1} 2s 500ms both;
+    }
   }
 `;
 
 export const IMG_Wrapper = styled.div`
   position: relative;
-  width: 150px;
-  height: 150px;
+  width: 158px;
+  height: 158px;
+  @media screen and (max-width: 359.98px) {
+    width: 150px;
+  }
 
   display: flex;
   justify-content: center;
   transform-origin: ${(props) => props.x} ${(props) => props.y};
-  &:hover {
+  &:hover&${BackgroundBlur} {
+    z-index: 101;
+  }
+  &:hover,
+  :focus {
     z-index: 100;
     background-color: transparent;
-    animation: ${zoomAnimation} 2s 500ms both;
+    scale: 2.1;
+
     box-shadow: 0px 0px 12px 12px rgba(13, 63, 124, 0.25);
   }
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 1440px) {
     width: 280px;
     height: 280px;
 
@@ -160,6 +232,11 @@ export const IMG_Wrapper = styled.div`
     background-repeat: no-repeat;
 
     &:hover {
+      ${BackgroundBlur} {
+        backdrop-filter: blur(8px);
+        width: 100vw;
+        height: 100vh;
+      }
       z-index: 100;
       background-color: transparent;
       animation: ${zoomAnimation} 2s 500ms both;
