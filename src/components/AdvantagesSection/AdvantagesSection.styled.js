8427d3ef0ab73aafca3be47a44d6ddae70563fe8
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-01-05 07:41:03 +0200

    added mob styles to advantage Section

--- src/components/AdvantagesSection/AdvantagesSection.styled.js
+++ src/components/AdvantagesSection/AdvantagesSection.styled.js
@@ -1,15 +1,35 @@
 import styled from "styled-components";
+export const Advantages_section = styled.div`
+  padding: 28px 0px;
+  margin-bottom: 0;
+`;
 
+export const Advantages_Container_Wrapper = styled.div`
+  padding: 0px 20px;
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+    padding: 40px 120px;
+    overflow: hidden;
+  }
+`;
 export const Advantages_items = styled.div`
+  display: grid;
+  gap: 20px;
+  justify-items: center;
+  /* grid-template-rows:    repeat(4, 100px); */
+  grid-template-columns: repeat(2, 1fr);
+  /* background-color: #ce5151; */
+  margin-top: 60px;
+  @media screen and (min-width: 1440px) {
     display: grid;
     gap: 40px;
     justify-items: center;
     /* grid-template-rows:    repeat(4, 100px); */
-grid-template-columns: repeat(3, 1fr);
-/* background-color: #ce5151; */
-margin-top: 60px;
-
-`
+    grid-template-columns: repeat(3, 1fr);
+    /* background-color: #ce5151; */
+    margin-top: 60px;
+  }
+`;
 export const Item = styled.div`
     /* background-color: #6a8f61; */
     /* width: 150px;
@@ -19,6 +39,14 @@ export const Item = styled.div`
     flex-direction: column;
     align-items: center;
 `
+export const H3 = styled.h3`
+  color: #0d3f7c;
+  font-family: Balsamiq Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
 export const Advantages_item_Img = styled.img`
 width: 120px;
     height: 120px;
@@ -28,6 +56,19 @@ margin-bottom: 12px;
 `
 
 export const TextContainer = styled.p`
-max-width: 380px;
-font-size: 18px;
-`
+  color: #0c151c;
+  font-family: Roboto;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 14.4/12;
+  @media screen and (min-width:768px) {
+   
+    font-size: 14px;
+    text-align: center;
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 380px;
+    font-size: 18px;
+  }
+`;
