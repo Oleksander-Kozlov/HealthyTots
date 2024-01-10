8eeac31feb6df74ba7ee005d8d2d4724ffd641d6
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-01-10 23:38:25 +0200

    fix styles

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -8,9 +8,11 @@ export const Main_Container = styled.main`
 
 
 export const Container_Wrapper = styled.div`
-width: 1440px;
-    padding:  40px 120px;
-    overflow: hidden;    
+  padding: 40px 120px;
+  overflow: hidden;
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+  }
 `;
 
 
