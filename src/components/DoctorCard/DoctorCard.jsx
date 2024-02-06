439ec5f02a889f7f1a4ac16e35e6152437db7f05
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-02-07 01:53:24 +0200

    remove path

--- src/components/DoctorCard/DoctorCard.jsx
+++ src/components/DoctorCard/DoctorCard.jsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/prop-types */
 
 import { CardContainer, DescriptionContainer, DocotorImg, Name, Text, Title } from "./DoctorCard.styled";
-import doc_img1 from "../../assets/doctorImg/doctor1.png";
-import doc_img2 from "../../assets/doctorImg/doctor2.png";
-import doc_img3 from "../../assets/doctorImg/doctor3.png";
-import doc_img4 from "../../assets/doctorImg/doctor4.png";
-import doc_img5 from "../../assets/doctorImg/doctor5.png";
 
 
-// eslint-disable-next-line react/prop-types
+
+
 const DoctorCard = ({ data }) => {
 
 
