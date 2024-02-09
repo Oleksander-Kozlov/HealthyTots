ff3bb422ed7d62b9a6751a20c94409ca5a15b17b
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-02-09 23:23:10 +0200

    Updates

--- assets/index-DR9qlG48.js
+++ assets/index-DR9qlG48.js
@@ -597,13 +597,13 @@ gap: 48px;
   /* Медиа-запрос для экранов с шириной до 768px */
   @media screen and (min-width: 768px) {
     width: 297px;
-    height: 706px;
+    height: auto;
   }
 
   /* Медиа-запрос для экранов с шириной больше 1440px */
   @media screen and (min-width: 1440px) {
     width: 588px;
-    height: 838px;
+    
   }
 `,Nx=()=>i.jsx(zx,{title:"Google Map",src:"https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d1316.9794807749117!2d29.241460956693786!3d48.68714926351284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d48.6874185!2d29.2421072!4m5!1s0x40cd710012bc9c05%3A0xf643471667f2104a!2zIkhlYWx0eSBUb3RzIiBwZWRpYXRyaWNzLCDQstGD0LvQuNGG0Y8g0J3QsNC60L7QvdC10YfQvdC-0LPQviwgMTMx0IQsINCb0LDQtNC40LbQuNC9LCDQktGW0L3QvdC40YbRjNC60LAg0L7QsdC70LDRgdGC0YwsIDI0MzIw!3m2!1d48.6865917!2d29.242666699999997!5e0!3m2!1suk!2sua!4v1703075511185!5m2!1suk!2sua",style:{border:0},allowFullScreen:"",loading:"lazy",referrerPolicy:"no-referrer-when-downgrade"}),Ux="/HealthyTots/assets/Unionbg@1x-LEEWv6kD.png",Hx="/HealthyTots/assets/Unionbg@2x-r1inn5u5.png",Vx=E.main`
   position: relative;
@@ -697,14 +697,14 @@ gap: 48px;
     align-items: stretch;
   }
   @media screen and (min-width: 1440px) {
-    margin-bottom: 48px;
+    
 
     gap: 126px;
     flex-direction: row;
     align-items: stretch;
 
     border-radius: 60px;
-    width: 1200px;
+   
   }
 `,Kx=E.div`
   padding: 24px 32px;
@@ -756,10 +756,11 @@ gap: 48px;
   font-size: 16px;
   @media screen and (min-width: 768px) {
     font-size: 24px;
+    margin: 0;
+    margin-bottom: 16px;
   }
 
   @media screen and (min-width: 1440px) {
-    margin-bottom: 16px;
     font-size: 24px;
   }
 `,m2=E.p`
