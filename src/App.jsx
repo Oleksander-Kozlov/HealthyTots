aa6bc60e2eb1ea5245a5ff4a92d5da1ce55e28a5
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-02-21 02:24:10 +0200

    fix path and routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,27 @@ import ContactsPage from "./pages/ContactsPage/ContactsPage";
 import DoctorsPage from "./pages/DoctorsPage/DoctorsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import ServicesPage from "./pages/Services/ServicesPage";
-import Service from "./components/Service/Service";
+import ServicesPage from "./pages/ServicesPage/ServicesPage";
+import ServiceTable from "./components/ServiceTable/ServiceTable"
+
 
 function App() {
   return (
     <AppWrapper>
-              <GlobalStyle />
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
           <Route path="doctors" element={<DoctorsPage />} />
           <Route path="services" element={<ServicesPage />} />
-          <Route path="services/:id" element={<Service />} />
+          <Route path="services/:id" element={<ServiceTable />} />
           <Route path="blog" element={<BlogPage />} />
           <Route path="blog/:id" element={<BlogDetailsPage />} />
           <Route path="contacts" element={<ContactsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
-      </AppWrapper>
+    </AppWrapper>
   );
 }
 
