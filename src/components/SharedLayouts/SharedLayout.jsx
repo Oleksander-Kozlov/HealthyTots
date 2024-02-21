58ea5f95ca93d22574994853f051d4a21d25d9da
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-02-21 02:32:12 +0200

    remove coments

--- src/components/SharedLayouts/SharedLayout.jsx
+++ src/components/SharedLayouts/SharedLayout.jsx
@@ -1,25 +1,18 @@
 import { Outlet } from "react-router-dom";
-import { Suspense } from 'react';
+import { Suspense } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const SharedLayout = () => {
   return (
     <>
-      {/* <div style={{height: "90px", backgroundColor: "black"}}></div>
-      <Header style={{ position: "sticky", top: "0", zIndex: "100", backgroundColor: "white" }}/> */}
-      <Header/>
-         <Suspense
-            //   fallback={<Loader />}
-          >
-              <Outlet />
-              
-          </Suspense>
-          <Footer/>
+      <Header />
+      <Suspense>
+        <Outlet />
+      </Suspense>
+      <Footer />
     </>
   );
 };
 
 export default SharedLayout;
-
-
